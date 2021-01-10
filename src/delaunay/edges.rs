47eed928f52f64a08ea53da30ab9ad00a83290e3
Martin <martinfrances107@hotmail.com>
2021-01-10 15:16:05 +0000

    BugFix: edge(). Fixed input to excess() function. Also now using extent() function as appropiate. Simplified dropped HashMap now using HashSet. Alsokey generation is faster just using raw tuple instead of string.

--- src/delaunay/edges.rs
+++ src/delaunay/edges.rs
@@ -2,51 +2,36 @@ use geo::Coordinate;
 use num_traits::Float;
 
 use super::excess::excess;
-use std::collections::HashMap;
+use rust_d3_array::extent::extent;
+
+use std::collections::HashSet;
 
 pub fn edges<T: Float>(triangles: &[Vec<usize>], point: &[Coordinate<T>]) -> Vec<[usize; 2]> {
     if point.len() == 2usize {
         return vec![[0usize, 1usize]];
     }
+    let zero = T::zero();
+    let mut h_index = HashSet::new();
 
-    let mut h_index = HashMap::new();
     for tri in triangles {
         if tri[0] == tri[1] {
-            return Vec::new();
+            continue;
         }
 
-        let ex_in = vec![point[0], point[1], point[2]];
+        let ex_in: Vec<Coordinate<T>> = tri.iter().map(|i| point[*i]).collect();
 
-        if excess(&ex_in) < T::zero() {
-            return Vec::new();
+        if excess(&ex_in) < zero {
+            continue;
         }
 
         for i in 0..3 {
             let j = (i + 1) % 3;
-            let code = format!("{}-{}", tri[i], tri[j]);
-            h_index.insert(code, true);
+            let code = extent(vec![tri[i], tri[j]], None);
+            h_index.insert(code);
         }
     }
 
-    let mut out: Vec<[usize; 2]> = Vec::new();
-    for key in h_index.keys() {
-        let a_split: Vec<&str> = key.split('-').collect();
-        out.push([a_split[0].parse().unwrap(), a_split[1].parse().unwrap()]);
-    }
+    let out = h_index.into_iter().collect::<Vec<_>>();
 
     return out;
 }
-
-// function geo_edges(triangles, points) {
-//   const _index = {};
-//   if (points.length === 2) return [[0, 1]];
-//   triangles.forEach(tri => {
-//     if (tri[0] === tri[1]) return;
-//     if (excess(tri.map(i => points[i])) < 0) return;
-//     for (let i = 0, j; i < 3; i++) {
-//       j = (i + 1) % 3;
-//       _index[extent([tri[i], tri[j]]).join("-")] = true;
-//     }
-//   });
-//   return Object.keys(_index).map(d => d.split("-").map(Number));
-// }
