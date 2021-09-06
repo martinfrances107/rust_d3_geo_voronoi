157e74b70f7e1eb2afb4db6f45358fc6bb5cdf14
Martin <martinfrances107@hotmail.com>
2021-09-06 14:54:01 +0100

    Minor: restricted visibility of cartesian. It is local to module. Renamed parameter to shadow the JS original.

--- src/delaunay/cartesian.rs
+++ src/delaunay/cartesian.rs
@@ -3,9 +3,9 @@ use geo::{CoordFloat, Coordinate};
 /// Converts spherical coordinates (degrees) to 3D Cartesian.
 /// Note there is a similar but different function  rust_d3_geo
 /// This only difference this one convert to radians first.
-pub fn cartesian<T: CoordFloat>(spherical: &Coordinate<T>) -> [T; 3] {
-    let lambda = spherical.x.to_radians();
-    let phi = spherical.y.to_radians();
+pub(super) fn cartesian<T: CoordFloat>(coordinates: &Coordinate<T>) -> [T; 3] {
+    let lambda = coordinates.x.to_radians();
+    let phi = coordinates.y.to_radians();
     let cos_phi = phi.cos();
     [cos_phi * lambda.cos(), cos_phi * lambda.sin(), phi.sin()]
 }
