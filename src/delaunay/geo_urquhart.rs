a41e77581109894c8342bb679119a12b2d51e00e
Martin <martinfrances107@hotmail.com>
2021-09-11 23:24:48 +0100

    Performance: geo_urquhart() data input into HasSet is a tupple not a formatted string.

--- src/delaunay/geo_urquhart.rs
+++ src/delaunay/geo_urquhart.rs
@@ -10,23 +10,23 @@ pub fn geo_urquhart<T: Float>(
     triangles: Rc<Vec<[usize; 3]>>,
 ) -> Box<dyn Fn(&Vec<T>) -> Vec<bool>> {
     Box::new(move |distances: &Vec<T>| {
-        let mut h_lengths: HashMap<String, T> = HashMap::new();
-        let mut h_urquhart: HashMap<String, bool> = HashMap::new();
+        let mut h_lengths: HashMap<(usize, usize), T> = HashMap::new();
+        let mut h_urquhart: HashMap<(usize, usize), bool> = HashMap::new();
 
         for (i, edge) in edges.iter().enumerate() {
-            let u = format!("{}-{}", edge[0], edge[1]);
-            h_lengths.insert(u.clone(), distances[i]);
+            let u = (edge[0], edge[1]);
+            h_lengths.insert(u, distances[i]);
             h_urquhart.insert(u, true);
         }
 
         triangles.iter().for_each(|tri| {
             let mut l = T::zero();
-            let mut remove: Option<String> = None;
+            let mut remove: Option<(usize, usize)> = None;
             for j in 0..3 {
                 // extent is used to order the two tri values  smallest to largest.
                 let e = extent(vec![tri[j], tri[(j + 1usize) % 3usize]], None);
 
-                let u = format!("{}-{}", e[0], e[1]);
+                let u = (e[0], e[1]);
                 if let Some(l_found) = h_lengths.get(&u) {
                     if *l_found > l {
                         l = *l_found;
@@ -41,7 +41,7 @@ pub fn geo_urquhart<T: Float>(
         let out: Vec<bool> = edges
             .iter()
             .map(|edge| {
-                let code: String = format!("{}-{}", edge[0], edge[1]);
+                let code = (edge[0], edge[1]);
                 return *h_urquhart.get(&code).unwrap();
             })
             .collect();
