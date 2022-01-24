c4be744ee78dab4215c098c10f4008cf593e8591
Martin <martinfrances107@hotmail.com>
2022-01-24 16:00:54 +0000

    profile_target - Produces a flamegraph from a complex computation.

--- README.md
+++ README.md
@@ -4,34 +4,34 @@
 
 This is a port of the [d3-geo-voronoi](<https://github.com/Fil/d3-geo-voronoi>) library into a [RUST](<https://www.rust-lang.org/>) library.
 
- It is in a very early development phase, but to show progress. The library can be used to compute the following delaunay mesh.
+ As a example the library can be used to compute the following delaunay mesh.
 ![Delaunay mesh from a set of random points on a sphere](./lamp.png "Delaunay mesh from a set of random points on a sphere")
 
 ( computed from 6,000 points on a sphere selected at random )
 
 Currently we have 82% test coverage ( as reported by cargo tarpaulin -o Html )
 
-## Phase 1: Porting
+## Performance Profiling.
 
-Early draft port - sub module by submodule. Sub module porting means the test have also been ported.
-No API stability guarentees.
+### Demo Page
+The original javascript library has a benchmark in the form of web page which records the number of frames displayed per second.
+( d3-geo_voronoi/src/benchmark/sphereCanvas.html)
+For comparison the bencmark has been ported
+( rust_d3_geo_vornoi/benchmark )
 
-## Phase 2: Performance Testing
+The result show only a minor 5-10% improvement in performance.
 
-API finialization. There maybe optimisation in the area of generics. So the API only gets locked down in phase 2.
- The code will be profiled and bottlenecks identified.
+### Profile Target
+This workspace contain a binary "profie_target" which outputs the result of a computation similar to that of the demo_page.
+Before API finialization - I am currently  using cargo flamegraph to identify any obvious optimizations.
 
-Modules, passing test ready for phase 2 evaluation :-
-
-## Phase 3: Other Development.
+## Further Development.
 
 To be ported:-
 
 contour.js and geo-contour-test.js
 geo_voronoi_test "geoVoronoi.hull does not break on difficult polygons"
 
-## Demo Page
-A demonstration webpage is supplied which acts as both a benchmark and a template for running this library in a browser.
 
 ## Instructions
 It used [wasm pack](<https://github.com/rustwasm/wasm-pack>) to package the following packages into a single wasm file. [rust_d3_geo](<https://github.com/martinfrances107/rust_d3_geo>), [rust_d3_delaunay](<https://github.com/martinfrances107/rust_d3_delaunay>). From this,  a simple demo application constructed.
@@ -51,16 +51,8 @@ the last command automatically start you web browser.
 
 * [`delaunator`]("https://github.com/mourner/delaunator-rs.git") for generation of the delaunay mesh.
 
-* [`console_error_panic_hook`](https://github.com/rustwasm/console_error_panic_hook)
-
 * [`rust_d3_array`]("https://github.com/martinfrances107/rust_d3_array.git") for a common set of helpers.
 
 * [`rust_d3_delaunay`]("https://github.com/martinfrances107/rust_d3_delaunay.git")
 
 * [`rust_d3_geo`]("https://github.com/martinfrances107/rust_d3_geo.git")
-
-* [`wasm-bindgen`](https://github.com/rustwasm/wasm-bindgen) for communicating between WebAssembly and JavaScript. For logging panic messages to the developer console.
-* [`wee_alloc`](https://github.com/rustwasm/wee_alloc), an allocator optimized
-  for small code size.
-
-* [`wasm-pack`](https://github.com/rustwasm/wasm-pack), A generator used to created all the glue code to create the rust benchamrk web-app.
---
+++ profile_target/src/main.rs
@@ -0,0 +1,126 @@
+use std::fs::{self, File};
+use std::io::prelude::*;
+use std::io::LineWriter;
+use std::iter::repeat_with;
+extern crate rand;
+use geo::Geometry::Polygon;
+
+use geo::{Coordinate, Geometry, MultiPoint};
+
+use rust_d3_geo::clip::circle::line::Line;
+use rust_d3_geo::data_object::FeatureCollection;
+use rust_d3_geo::path::builder::Builder as PathBuilder;
+use rust_d3_geo::projection::orthographic::Orthographic;
+use rust_d3_geo::projection::{Raw, Rotate};
+use rust_d3_geo::stream::StreamDrainStub;
+
+use rust_d3_geo_voronoi::voronoi::GeoVoronoi;
+
+fn draw() -> String {
+    // size is the number of voronoi
+    let size = 6000;
+    // let s = path.object(&object);
+    let ortho_builder = Orthographic::builder();
+
+    let sites = MultiPoint(
+        repeat_with(rand::random)
+            .map(|(x, y): (f64, f64)| {
+                Coordinate {
+                    x: 360_f64 * x,
+                    y: 180_f64 * y - 90_f64,
+                }
+                .into()
+            })
+            .take(size as usize)
+            .collect(),
+    );
+
+    let mut gv: GeoVoronoi<StreamDrainStub<f64>, Line<f64>, f64> =
+        GeoVoronoi::new(Some(Geometry::MultiPoint(sites.clone())));
+
+    let ortho = ortho_builder.rotate(&[0_f64, 0_f64, 0_f64]).build();
+    let mut path = PathBuilder::context_pathstring().build(ortho);
+
+    // TODO can this be defined statically
+    let scheme_category10: [String; 10] = [
+        String::from("#1f77b4"),
+        String::from("#ff7f0e"),
+        String::from("#2ca02c"),
+        String::from("#d62728"),
+        String::from("#9467bd"),
+        String::from("#8c564b"),
+        String::from("#e377c2"),
+        String::from("#7f7f7f"),
+        String::from("#bcbd22"),
+        String::from("#17becf"),
+    ];
+
+    match gv.polygons(None) {
+        None => {
+            panic!("failed to get polygons");
+        }
+        Some(FeatureCollection(fc)) => {
+            let mut paths = String::from("");
+            for (i, features) in fc.iter().enumerate() {
+                let d = match &features.geometry[0] {
+                    Polygon(polygon) => {
+                        // todo
+                        path.object(&Geometry::Polygon(polygon.clone()))
+                    }
+                    _ => {
+                        panic!("polygon not found");
+                    }
+                };
+                let line = format!(
+                    "<path d={:?} fill=\"{}\" stroke=\"white\" /> </path> ",
+                    d,
+                    scheme_category10[i % 10]
+                );
+                paths.push_str(&line);
+            }
+            paths
+        }
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let file = File::create("profile_output.html")?;
+    let mut file = LineWriter::new(file);
+
+    let header = b"
+    <!DOCTYPE html>
+    <html lang=\"en\">
+    <head>
+    <title>Profile Target</title>
+    <meta charset=\"utf-8\">
+    <meta name=\"description\" content=\"Complex output used for profiling.\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+    </head>
+    <body>
+    <h1>Project: rust_d3_geo_voronoi</h1>
+    <p>
+     A Complex SVG used for profiling.
+     </p>
+     <?xml version=\"1.0\" standalone=\"no\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">
+    <svg version=\"1.1\"
+      width=\"1280\"
+      height=\"640\"
+      viewBox=\"0 0 1200 518\"
+      xmlns=\"http://www.w3.org/2000/svg\"
+    >";
+
+    file.write_all(header)?;
+
+    let d_string = draw();
+    let d = d_string.as_bytes();
+
+    file.write_all(&d)?;
+
+    let tail = b"</svg></body></html>";
+
+    file.write_all(tail)?;
+
+    file.flush()?;
+
+    Ok(())
+}
