454f82367bbfe755f5fa77402b3cf64200e9693b
martinfrances107 <martinfrances107@hotmail.com>
2024-07-27 07:46:52 +0100

    Minor: clippy fix.

--- profile_target/src/main.rs
+++ profile_target/src/main.rs
@@ -28,7 +28,7 @@ use d3_geo_rs::projection::RotateSet;
 use d3_geo_voronoi_rs::voronoi::ConstructionError;
 use d3_geo_voronoi_rs::voronoi::Voronoi;
 
-static SCHEME_CATEGORY10: [&'static str; 10] = [
+static SCHEME_CATEGORY10: [&str; 10] = [
     "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b",
     "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
 ];
